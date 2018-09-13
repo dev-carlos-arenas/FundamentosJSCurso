9664e1fc26ec89772edd39257831cbdeb0eedc15
dev-carlos-arenas <dev.carlos.arenas@outlook.com>
2018-09-13 17:27:11 -0500

    Nuevo ajuste

--- clase_1/js/script.js
+++ clase_1/js/script.js
@@ -3,5 +3,5 @@ var name = "Carlos", lastName= "Arenas";
 var nameInUpperCase = name.toUpperCase();
 
 
-console.log(name.charAt(name.length - 1));
+console.log(name.charAt(0));
 console.log(name.substr(-1));
--- hyperlinks.js
+++ hyperlinks.js
@@ -3,7 +3,7 @@ const nombreClase = [
 ];
     document.write('<table><tr><td><ul>');
 for(i = 0; i <= 31; i++){
-    document.write(`<li><a href="/clase_${i+1}/clase.html">Clase ${i+1} - ${nombreClase[i]}</a></li>`);
+    document.write(`<li><a href="/FundamentosJSCurso/clase_${i+1}/clase.html">Clase ${i+1} - ${nombreClase[i]}</a></li>`);
     if(parseInt((i+1)%10) === 0){
         document.write(`</ul></td><td><ul>`);
     }
