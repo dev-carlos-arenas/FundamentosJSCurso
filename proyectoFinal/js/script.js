343c6b29464f141da86dbd64249b755e76a270c5
dev-carlos-arenas <dev.carlos.arenas@outlook.com>
2018-09-17 13:22:23 -0500

    proyecto final

--- proyectoFinal/js/script.js
+++ proyectoFinal/js/script.js
@@ -4,6 +4,8 @@ const naranja = document.getElementById('naranja');
 const verde = document.getElementById('verde');
 const btnEmpezar = document.getElementById('btnEmpezar');
 
+var secuencia = Math.round(Math.random((4-1)+1));
+
 class Juego {
   constructor() {
     this.inicializar();
